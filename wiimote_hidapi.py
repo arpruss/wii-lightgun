d2bcfdf3f38f9c8f17ef3f2f95d2cf4334ed7314
Alexander Pruss <arpruss@gmail.com>
2026-09-01 08:53:31 -0500

    foo

---
+++ wiimote_hidapi.py
@@ -0,0 +1,198 @@
+import hid
+import time
+from threading import Thread
+
+WIIUSE = False
+WIIMOTE_VID = 0x057e
+WIIMOTE_PIDS = [0x0306, 0x0330]
+
+RW_EEPROM = 0
+RW_REG = 0x04
+
+
+EVENT_DT = .01
+
+set_ir_sensitivity = None
+NEVER_CONTINUOUS = True
+IR_CALIBRATION_LOCATIONS = ( ((0,2,4),(1,2,6)),  # X1,Y1
+                             ((3,2,0),(4,2,2)),  # X2,Y2
+                             ((5,7,4),(6,7,6)),  # X3,Y3
+                             ((8,7,0),(9,7,2)) ) # X4,Y4 
+                             
+IR_LEVELS = ( (b"\x02\x00\x00\x71\x01\x00\x64\x00\xFE", b"\xFD\x05"),
+              (b"\x02\x00\x00\x71\x01\x00\x96\x00\xB4", b"\xB3\x04"),
+              (b"\x02\x00\x00\x71\x01\x00\xAA\x00\x64", b"\x63\x03"),
+              (b"\x02\x00\x00\x71\x01\x00\xC8\x00\x36", b"\x35\x03"),
+              (b"\x02\x00\x00\x71\x01\x00\x72\x00\x20", b"\x1F\x03") )
+
+ACCEL_0G_CALIBRATION_LOCATIONS = ( (0,3,4), (1,3,2), (2,3,0) )
+ACCEL_1G_CALIBRATION_LOCATIONS = ( (4,7,4), (5,7,2), (6,7,0) )
+
+CALIBRATION_OFFSET = 0
+IR_CALIBRATION_OFFSET_1 = 0
+IR_CALIBRATION_OFFSET_2 = 11
+IR_CALIBRATION_SIZE = 11
+ACCEL_CALIBRATION_OFFSET = 2 * IR_CALIBRATION_SIZE
+ACCEL_CALIBRATION_SIZE = 10
+CALIBRATION_SIZE = 2 * IR_CALIBRATION_SIZE + ACCEL_CALIBRATION_SIZE
+
+def parseIRCalibration(data):
+    if len(data) < 11:
+        return None
+    s = (0x55 + sum(data[i] & 0xFF for i in range(10))) & 0xFF
+    if s != (data[10] & 0xFF):
+        return None
+    def getCoordinate(bits07_offset,bits89_offset,bits89_shift):
+        return (data[bits07_offset] & 0xFF) | (((data[bits89_offset] >> bits89_shift) & 0x3) << 8)    
+    
+    return tuple( (getCoordinate(*icl[0]), getCoordinate(*icl[1])) for icl in IR_CALIBRATION_LOCATIONS)
+ 
+def parseAccelCalibration(data):
+    if len(data) < 10:
+        return None
+    s = (0x55 + sum(data[i] & 0xFF for i in range(9))) & 0xFF
+    if s != (data[9] & 0xFF):
+        return None
+    def getCoordinate(bits29_offset,bits01_offset,bits01_shift):
+        return ((data[bits29_offset] & 0xFF) << 2) | ((data[bits01_offset] >> bits01_shift) & 0x3)    
+        
+    accel0g = tuple( getCoordinate(*cl) for cl in ACCEL_0G_CALIBRATION_LOCATIONS )
+    accel1g = tuple( getCoordinate(*cl) for cl in ACCEL_1G_CALIBRATION_LOCATIONS )
+    
+    return accel0g, accel1g
+
+def setCalibration(wm,buffer):
+    irc = parseIRCalibration(buffer[IR_CALIBRATION_OFFSET_1:])
+    if not irc:
+        irc = parseIRCalibration(buffer[IR_CALIBRATION_OFFSET_2:])
+    if irc:
+        wm.irCalibration = irc
+    acc = parseAccelCalibration(buffer[ACCEL_CALIBRATION_OFFSET:])
+    if acc:
+        wm.accel0gCalibration = acc[0]
+        wm.accel1gCalibration = acc[1]
+        
+def getWord(data, offset):
+    return (data[offset] & 0xFF) << 8 | (data[offset+1] & 0xFF)
+
+class MyWiimote:
+    def __init__(self):
+        target_path = None
+        while not target_path:
+            for dev in hid.enumerate():
+                if dev['vendor_id'] == WIIMOTE_VID and dev['product_id'] in WIIMOTE_PIDS:
+                    target_path = dev['path']
+                    print(f"Found Wiimote at: {target_path}")
+                    break
+            time.sleep(0.5)
+
+        # Open the hidraw device path
+        self.handle = hid.device()
+        self.handle.open_path(target_path)
+        self.handle.set_nonblocking(True)
+        self.timeout = 5
+        self.callback = lambda data,t: None
+        
+        self.calibrate()
+        
+    @property
+    def led(self):
+        return self._leds
+    
+    @led.setter
+    def led(self, l):
+        self.handle.write((0x10,l))
+        self._leds = l
+        
+    def read_sync(self,location,address,size):
+        output = b''
+        if size == 0:
+            return output
+        read_cmd = bytes((0x17, location, (address&0xFF0000)>>16, (address&0xFF00)>>8, address&0xFF, (size&0xFF00)>>8, size&0xFF))
+        self.handle.write(read_cmd)
+        time.sleep(0.2)
+        t = time.monotonic()
+        while time.monotonic() <= t + self.timeout and size > 0:
+            data = self.handle.read(64)
+            if data and data[0] == 0x21 and data[3] & 0xF == 0 and  len(data)>6 and getWord(data, 4) == address:
+                output += bytes(data[6:])
+                n = min(size, len(data)-6)
+                address += n
+                size -= n
+            else:
+                time.sleep(0.01)
+        if size == 0:
+            return output
+        else:
+            return None
+    
+    def write_reg(self,address,data):
+        if len(data) > 16:
+            return
+        paddedData = list(data) + (16-len(data)) * [0,]
+        write_cmd = bytes([0x16, RW_REG, (address&0xFF0000)>>16, (address&0xFF00)>>8, address&0xFF, len(data)] + paddedData)
+        self.handle.write(write_cmd)
+        time.sleep(0.1)
+            
+    def enable(self,mode,irLevel=3): # mode is ignored
+        self.listenThread = Thread(target = self.listening, args=(mode,irLevel,))
+        self.listenThread.start()
+    
+    def listening(self,mode,irLevel):
+        # enable camera in extended data mode
+        for i in range(5):
+            self.handle.write(bytes((0x13, 0x04)))
+            self.handle.write(bytes((0x1A, 0x04)))
+            self.write_reg(0xb00030,(0x08,))
+            irLevel = max(min(irLevel,5),1)
+            self.write_reg(0xb00000,IR_LEVELS[irLevel][0])
+            self.write_reg(0xb0001a,IR_LEVELS[irLevel][1])
+            self.write_reg(0xb00033,(3,)) # extended
+            #self.write_reg(0xb00030,(0x08,))
+            self.handle.write(bytes((0x12, 0x04, 0x37))) # continuous reporting
+        while True:
+            data = self.handle.read(18)
+            if data and data[0] == 0x37 and len(data) == 18:
+                print(data)
+                t = time.monotonic()
+                buttons = getWord(data,1)
+                out = {"buttons": buttons & 0x1F9F}
+                x = (0xFF & data[3]) << 2 | (3&(buttons >> 13))
+                y = (0xFF & data[4]) << 2 | (2&(buttons >> 4))
+                z = (0xFF & data[5]) << 2 | (2&(buttons >> 5))
+                out["raw_acc"] = (x,y,z)
+                out["acc"] = ( (x-self.accel0gCalibration[0])/self.accel1gCalibration[0],
+                               (y-self.accel0gCalibration[1])/self.accel1gCalibration[1],
+                               (z-self.accel0gCalibration[2])/self.accel1gCalibration[2] )
+                offset = 6
+                irData = []
+                for i in range(4):
+                    x = (data[offset]&0xFF) | ((data[offset+2]&0x30)>>4) << 8
+                    y = (data[offset+1]&0xFF) | ((data[offset+2]&0xC0)>>6) << 8
+                    if y < 1023:
+                        s = data[offset+2] & 0xF
+                        irData += ((x,y),s)
+                        print("have")
+                    else:
+                        irData += (None,)
+                    offset += 3
+                out["ir"] = irData
+                print(out)
+                self.callback(out,t)
+            time.sleep(EVENT_DT)
+        
+    def calibrate(self):
+        data = self.read_sync(RW_EEPROM, CALIBRATION_OFFSET, CALIBRATION_SIZE)
+        if data:
+            setCalibration(self, data)
+    
+
+if __name__=='__main__':
+    w = MyWiimote()
+    print(w.irCalibration)
+    print(w.accel0gCalibration)
+    print(w.accel1gCalibration)
+    w.enable(0)
+    while True:
+        time.sleep(1)
+        pass
